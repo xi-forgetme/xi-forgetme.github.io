b2532ac8b40620547babd899909c0197137d96a4
曦xi <secretboy.smr@gmail.com>
2023-01-03 00:46:13 -0800

    upload

--- js/index.js
+++ js/index.js
@@ -3,4 +3,18 @@ document.onreadystatechange = () => {
         // document ready
     }
 };
-  
+
+//Right-click context menu
+document.addEventListener('DOMContentLoaded', function(){
+    var rightMenu = document.getElementById('right-menu')
+    document.oncontextmenu = function(event){
+        event.preventDefault();
+        rightMenu.style.display = "none"
+        rightMenu.style.display = "block"
+        rightMenu.style.left = event.offsetX + 'px'
+        rightMenu.style.top = event.offsetY + 'px'
+    }
+    document.onclick = function(event){
+        rightMenu.style.display = "none"
+    }
+})
